7d768d257ef170c6a8642b5d1e7d5c94062032a5
Dima Kononenko <dimakononenko@dimas-mbp.attlocal.net>
2018-11-10 01:59:26 -0800

    Media Query first tries

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,16 +9,16 @@ export default class Header extends React.Component {
       <header className="container">
         <nav>
           <img src={img} id="logo"/>
-          <ul>
-            <li><a ahref="#">Home</a></li>
-            <li><a ahref="#">Products</a></li>
-            <li><a ahref="#">Team</a></li>
-            <li><a ahref="#">Blog</a></li>
-            <li><a ahref="#">Contact</a></li>
+          <ul className="nav">
+            <li><a className="nav-item" ahref="#">Home</a></li>
+            <li><a className="nav-item" ahref="#">Products</a></li>
+            <li><a className="nav-item" ahref="#">Team</a></li>
+            <li><a className="nav-item" ahref="#">Blog</a></li>
+            <li><a className="nav-item" ahref="#">Contact</a></li>
           </ul>
-        </nav>  
-        <button href="#!" className="button" data-micron="bounce">Sign Up</button>
-        </header>
+          </nav>  
+          <button href="#!" className="button" data-micron="bounce">Sign Up</button>
+      </header>
     )
   }  
 };
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/Header';
 import FirstScreen from './components/FirstScreen';
+import SecondSection from './components/SecondSection';
 import './styles/styles.scss';
 
 
@@ -9,6 +10,7 @@ const jsx = (
   <div>
     <Header />
     <FirstScreen />
+    <SecondSection />
   </div>
 )
 

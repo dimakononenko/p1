5648922315db064677484407a71371fae67884ca
Dima Kononenko <dimakononenko@dimas-mbp.attlocal.net>
2018-11-15 00:10:16 -0800

    Header styled for all Media Queries with styled-components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import img from '../../images/logo.png';
 import main_illustration from '../../images/main_illustration.png';
-import { HeaderContainer, Logo, Button, Img } from './Header.styles';
+import { HeaderContainer, Logo, Button, Img, Ul, Li, A } from './Header.styles';
 
 
 // Using of styled-components
 export default () => (
   <HeaderContainer>
     <Logo src={img} id="logo"/>
-    <ul>
-      <li><a ahref="#">Home</a></li>
-      <li><a ahref="#">Products</a></li>
-      <li><a ahref="#">Team</a></li>
-      <li><a ahref="#">Blog</a></li>
-      <li><a ahref="#">Contact</a></li>
-    </ul>
+    <Ul>
+      <Li><A ahref="#">Home</A></Li>
+      <Li><A ahref="#">Product</A></Li>
+      <Li><A ahref="#">Team</A></Li>
+      <Li><A ahref="#">Blog</A></Li>
+      <Li><A ahref="#">Contact</A></Li>
+    </Ul>
     <Button data-micron="bounce">Sign Up</Button>
     <Img src={main_illustration} id="illustration"></Img>
   </HeaderContainer>
--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -5,70 +5,135 @@ import styled from 'styled-components';
 // Styled-Components
 
 export const HeaderContainer = styled.div`
-  /* border: #000000 solid; */
-  display: flex;
-  align-items: flex-start;
-  justify-content: space-between;
-  ul {
-    display: flex;
-  }
-  li {
-    list-style: none;
-    margin: 8px 25px 25px;
-  }
   @media ${device.mobile} {
-    #logo {
-      display: none;
-    }
-    ul {
-      flex-direction: column;
-      margin-top: 25px;
-    }
-    li {
-      list-style: none;
-      margin: 0px 10% 15px;
-    }
-  }
-
-  @media ${device.tablet} {
-    border: #000000 solid;
-    font-family: Chivo;
-    font-size: 1em;
-    color: #000000;
     display: flex;
     align-items: flex-start;
     justify-content: space-between;
-    padding-top: 45px;
-    #logo {
-      display: flex;
-    }
-    ul {
-      flex-direction: column;
-    }
+    font-family: Chivo;
+    font-size: 1.2em;
+    color: #000000;
+  }
+  @media ${device.tablet} {
+  }
+  @media ${device.laptop} {
+    font-size: 1em;
   }
 `
 export const Logo = styled.img`
-    src: url(${props => props.src});
-    margin-left: 13%;
-
-`
-export const Button = styled.button`
-  border: 1px solid #FFFFFF;
-  font-family: Chivo-Regular;
-  height: 48px;
-  width: 128px;
-  text-align: center;
-  font-size: 16px;
-  color: #F1F8E9;
-  border-radius: 5px;
-  background: none;
-  position: relative;
-  margin: 20px 10% 10px;
+  src: url(${props => props.src});
   @media ${device.mobile} {
-    height: 40px;
-    width: 100px;
+    display: none;
+  }
+  @media ${device.tablet} {
+    display: flex;
+    margin-top: 45px;
+    margin-left: 40px;
+  }
+  @media ${device.laptop} {
+    right: 0;
+    margin-left: 120px;
+    /* margin-right: 113px; */
   }
 `
+
+export const Ul = styled.ul`
+  @media ${device.mobile} {
+    flex-direction: column;
+    margin-top: 57px;
+  }
+  @media ${device.tablet} {
+    position: relative;
+    right: 100px;
+  }
+  @media ${device.laptop} {
+    display: flex;
+    flex-direction: row;
+    margin-left: 100px;
+    position: relative;
+    right: 150px;
+  }
+`
+
+export const Li = styled.li`
+  @media ${device.mobile} {
+    list-style: none;
+    margin: 0px 16px 15px;
+    margin-right: 32px
+  }
+  @media ${device.tablet} {
+  }
+  @media ${device.laptop} {
+  }
+`
+export const A = styled.a`
+  @media ${device.mobile} {
+    position: relative;
+    text-decoration: none;
+    padding-bottom: 8px;
+    &:before,
+    &:after {
+      content: '';
+      position: absolute;
+      bottom: 2px;
+      left: 0;
+      right: 0;
+      height: 2px;
+      background-color: rgb(72, 189, 76);
+    }
+    &:before {
+      opacity: 0;
+      transform: translateY(- 8px);
+      transition: transform 0s cubic-bezier(0.175, 0.885, 0.320, 1.275), opacity 0s;
+    }
+    &:after {
+      opacity: 0;
+      transform: translateY(8px/2);
+      transition: transform .2s cubic-bezier(0.175, 0.885, 0.320, 1.275), opacity .2s;
+    }
+    &:hover,
+    &:focus {
+      &:before,
+      &:after {
+        opacity: 1;
+        transform: translateY(0);
+      }
+      &:before {
+        transition: transform .2s cubic-bezier(0.175, 0.885, 0.320, 1.275), opacity .2s;
+      }
+      &:after {
+        transition: transform 0s .2s cubic-bezier(0.175, 0.885, 0.320, 1.275), opacity 0s .2s;
+      }
+    }
+  }
+  @media ${device.tablet} {
+  }
+  @media ${device.desktop} {
+  }
+`
+export const Button = styled.button`
+  @media ${device.mobile} {
+    border: 1px solid #FFFFFF;
+    font-family: Chivo-Regular;
+    min-height: 40px;
+    min-width: 110px;
+    text-align: center;
+    font-size: 16px;
+    color: #F1F8E9;
+    border-radius: 5px;
+    background: none;
+    position: relative;
+    margin: 20px 10% 10px;
+    margin-top: 45px;
+  }
+  @media ${device.tablet} {
+    /* order: 2; */
+    height: 48px;
+    width: 128px;
+  }
+  @media ${device.laptop} {
+  }
+`
+
 export const Img = styled.img`
   position: absolute;
   top: 0;
@@ -76,7 +141,4 @@ export const Img = styled.img`
   z-index: -1;
   max-width: 660px;
   width: 60%;
-  @media ${device.mobileS} {
-    position: absolute
-  }
 `
